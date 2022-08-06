d3c1c4451a3b201029bfd0336380139c2a884835
Javalind <65702538+Javalind@users.noreply.github.com>
2022-08-06 15:56:27 +1200

    created base app, connected with api and made search bar
    Signed-off-by: Javalind <65702538+Javalind@users.noreply.github.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import axios from 'axios';
+import { useState } from 'react';
+import IconButton from "@mui/material/IconButton";
+import SearchIcon from "@mui/icons-material/Search";
+import TextField from "@mui/material/TextField";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    const [githubName, setGithubName] = useState("");
+    const [githubInfo, setGithubInfo] = useState<undefined | any>(undefined);
+
+    const GITHUB_BASE_URL = "https://api.github.com";
+
+    return (
+      <div>
+        <h1>
+          Github Search
+        </h1>
+        
+        <div>
+        <TextField
+          id="search-bar"
+          className="text"
+          value={githubName}
+          onChange={(prop: any) => {
+            setGithubName
+      (prop.target.value);
+          }}
+          label="Enter a Github username..."
+          variant="outlined"
+          placeholder="Search..."
+          size="small"
+        />
+        <IconButton
+          aria-label="search"
+          onClick={() => {
+            search();
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <SearchIcon style={{ fill: "blue" }} />
+        </IconButton>
+      </div>
+
+  
+        <p>
+          You have entered {githubName}
+        </p>
+          {githubInfo === undefined ? (
+            <p>Github not found</p>
+          ) : (
+            <div id="Github-result">
+              <img src={githubInfo.login} />
+            </div>
+          )}
+      </div>
+  );   
+
+    function search(){
+      axios.get(GITHUB_BASE_URL + "/users/" + githubName.toLowerCase())
+      .then((res) => {
+        setGithubInfo(res.data);
+      })
+      .catch((err) => {
+        console.log("Github not found");
+        setGithubInfo(undefined);
+      });
+  }
 }
 
 export default App;
